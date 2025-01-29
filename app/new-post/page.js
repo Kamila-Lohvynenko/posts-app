5bf58436c2f68217914141f815ae5a1f381c0b97
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2025-01-29 21:49:44 +0100

    handle errors

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,28 +1,18 @@
-import { redirect } from "next/navigation";
-import { storePost } from "@/lib/posts";
+"use client";
+
+// import { useFormState } from "react-dom";
+
 import FormSubmit from "@/components/form-submit";
+import { createPost } from "@/actions/posts";
+import { useActionState } from "react";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
+  const [state, formAction] = useActionState(createPost, {});
 
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -40,9 +30,16 @@ export default function NewPostPage() {
           <label htmlFor="content">Content</label>
           <textarea id="content" name="content" rows="5" />
         </p>
-        <p className="form-actions">
+        <div className="form-actions">
           <FormSubmit />
-        </p>
+        </div>
+        {state.errors && (
+          <ul className="form-errors">
+            {state.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
       </form>
     </>
   );
